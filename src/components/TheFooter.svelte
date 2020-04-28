8102c561a056c51ab5b20ceee71b848656757635
Fenny <25108519+Fenny@users.noreply.github.com>
2020-04-28 23:51:49 +0200

    Update TheFooter.svelte

--- src/components/TheFooter.svelte
+++ src/components/TheFooter.svelte
@@ -1,25 +1,9 @@
-<footer>
-  <div class="mid">
-    Copyright (c) 2019-present
-    <a href="https://github.com/fenny" target="_blank">Fenny</a> and
-    <a href="https://github.com/gofiber/fiber/graphs/contributors" target="blank">Fiber Contributors</a>.
-    Fiber is free and open-source software licensed under the
-    <a href="https://github.com/gofiber/fiber/blob/master/LICENSE" target="_blank">MIT License</a>.
-    Official logo was created by
-    <a href="https://github.com/koddr" target="_blank">Vic Shóstak</a>
-    and distributed under
-    <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons</a>
-    license (CC BY-SA 4.0 International). Fiber Documentation is hosted by  <a href="https://gitbook.com" target="_blank">GitBook.com</a>. Website by
-    <a href="https://github.com/jozsefsallai" target="_blank">József Sallai</a>.
-  </div>
-</footer>
-
 <style>
   footer {
     text-align: center;
     background: var(--accent-darker);
-    padding: 2em .5em;
-    font-size: .8em;
+    padding: 2em 0.5em;
+    font-size: 0.8em;
     color: #fff;
   }
 
@@ -31,3 +15,33 @@
     color: #fff;
   }
 </style>
+
+<footer>
+  <div class="mid">
+    Copyright (c) 2019-present
+    <a href="https://github.com/fenny" target="_blank">Fenny</a>
+    and
+    <a
+      href="https://github.com/gofiber/fiber/graphs/contributors"
+      target="blank">
+      Contributors
+    </a>
+    . Fiber is free and open-source software licensed under the
+    <a
+      href="https://github.com/gofiber/fiber/blob/master/LICENSE"
+      target="_blank">
+      MIT License
+    </a>
+    . Official logo was created by
+    <a href="https://github.com/koddr" target="_blank">Vic Shóstak</a>
+    and distributed under
+    <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">
+      Creative Commons
+    </a>
+    license (CC BY-SA 4.0 International). Fiber Documentation is hosted by
+    <a href="https://gitbook.com" target="_blank">GitBook.com</a>
+    . Website by
+    <a href="https://github.com/jozsefsallai" target="_blank">József Sallai</a>
+    .
+  </div>
+</footer>
